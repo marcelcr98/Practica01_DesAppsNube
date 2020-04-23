67e344f092514fd72f51ce53daa7ee2c13d8039c
marceloarmando <mcuellarr98@gmail.com>
2020-04-23 12:17:30 -0500

    commit 1

---
+++ src/config/dbConnection.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql');
+
+module.exports = () => {
+  return mysql.createConnection({
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'bda87bde2321a4',
+    password: '51ad6e9a',
+    database: 'heroku_df2f4a10ff3ea9d'
+  });
+}
