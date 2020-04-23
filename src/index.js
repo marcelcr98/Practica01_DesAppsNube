56ffe67c3eaebec9f423fdfc6879cd723bc140ca
marceloarmando <mcuellarr98@gmail.com>
2020-04-23 18:44:33 -0500

    commit 8

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ require('./app/routes/login.js')(app);
 
 //Server
 
-app.listen(app.get('port'), () =>{
-    console.log('OK',app.get('port'));
+app.listen(app.post('port'), () =>{
+    console.log('OK',app.post('port'));
 });
